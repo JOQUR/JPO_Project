ec3f4d77510f974c642559f3cb617f65e8bb2aa2
kierzek <kierzek@student.agh.edu.pl>
2022-12-06 20:32:03 +0100

    feat: Added CMAKE

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/PKier/OneDrive/Pulpit/JPO Projekt/JPO_Project
+# Build directory: C:/Users/PKier/OneDrive/Pulpit/JPO Projekt/JPO_Project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
